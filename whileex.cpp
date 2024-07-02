1b42eaef7095bd7cbd6016ac922deeafb4508333
GabrielVicente12 <166388633+GabrielVicente12@users.noreply.github.com>
2024-07-02 20:26:25 -0300

    Whilecpp

---
+++ whileex.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+int contador = 0;
+
+while(contador < 5)
+{
+    cout << "contador =" << contador << endl;
+    contador = contador + 1;
+}
+cout << "ACABOU!" << endl;
+
+return 0;
+}
